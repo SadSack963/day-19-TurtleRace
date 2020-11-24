b8d2b487356638e5398a70a539fb45230ff2c18b
john <john@jpatmore.plus.com>
2020-11-24 22:22:38 +0000

    Final version 1.0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-from random import choice
+from random import randint, choice
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 turtles = []
@@ -12,13 +12,32 @@ s.setup(width=500, height=400)
 
 # Turtle setup
 for i in range(0, len(colors)):
-    turtles.append({"turtle": Turtle(shape="turtle"), "position": start_position})
+    turtles.append({"color": colors[i], "turtle": Turtle(shape="turtle"), "position": start_position})
     turtles[i]["turtle"].pu()
-    turtles[i]["turtle"].color(colors[i])
+    turtles[i]["turtle"].speed(1)
+    turtles[i]["turtle"].color(turtles[i]["color"])
     turtles[i]["turtle"].goto(turtles[i]["position"], (i * 50) - 150)
 
-choice = s.textinput(title="Turtle Race", prompt="Which turtle do you think will win the race? : ").lower()
+race_running = False
+choices = f"{colors[0]}"
+for i in range(1, len(colors)):
+    choices += "/" + colors[i]
+user_choice = s.textinput(title="Turtle Race", prompt=f"Which turtle do you think will win the race?\n{choices} : ").lower()
+if user_choice:
+    race_running = True
+
+while race_running:
+    for turtle in turtles:
+        i = randint(0, 10)
+        turtle["turtle"].fd(i)
+        turtle["position"] += i
+        if turtle["position"] >= 230:
+            race_running = False
+            print(f"The {turtle['color']} turtle won the race")
+            if turtle["color"] == user_choice:
+                print(f"You won!")
+            else:
+                print(f"You lost. You chose the {user_choice} turtle.")
 
 
-
-s.exitonclick()
+# s.exitonclick()
